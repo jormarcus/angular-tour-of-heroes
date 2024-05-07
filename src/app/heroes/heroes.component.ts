da6b6c5e5bc10b9760c7e842db73a1f768d52032
jormarcus <jormarcus92@gmail.com>
2024-05-06 22:53:03 -0400

    feat: heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,20 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mocks/mock-heroes';
 import { Hero } from '../types/hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
